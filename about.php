f8e83400ef7b189ca21e290b994cc87b72f41f80
Viktoras Domarkas <vidomarkas@gmail.com>
2020-03-07 18:39:37 +0000

    create about page header

---
+++ about.php
@@ -0,0 +1,23 @@
+<?php /* Template Name: about */ ?>
+
+<?php get_header();?>
+
+<header class="about__header">
+    <div class="about__header__container">
+        <div class="about__header__person">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/intro-with-people--green-coat.png" alt=""></div>
+        <div class="about__header__text">
+            <h1 class="heading heading__primary--about underline underline--purple">Catalyx are consumer research
+                specialists who harness the Power of the Crowd to build better brands worldwide.</h1>
+        </div>
+        <div class="about__header__person about__header__person--right">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/intro-with-people--hand-in-pockets.png" alt="">
+        </div>
+    </div>
+
+</header>
+
+<main class="about"></main>
+
+
+<?php get_footer();?>
