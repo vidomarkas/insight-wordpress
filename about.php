45962ab160af25f354439e2c543997693fb2b9dc
Viktoras Domarkas <vidomarkas@gmail.com>
2020-03-08 05:34:04 +0000

    create about page, add team photos and grid

--- about.php
+++ about.php
@@ -17,7 +17,149 @@
 
 </header>
 
-<main class="about"></main>
+<main class="about">
+
+    <h2 class="heading heading__primary--about underline underline--purple">Meet the minds shaping an industry.</h2>
+    <div class="about-grid">
+        <div class="about-grid__item about-card ">
+            <div class="about-card__side about-card__side--front">
+                <div class="about-card__image"
+                    style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/person1.jpg)"></div>
+                <h3 class="about-card__name">Harry Gibson</h3>
+                <p class="about-card__job-title">Founder / Global digital director</p>
+            </div>
+            <div class="about-card__side about-card__side--back">
+                <div class="about-card__details">
+
+                    <p>Harry is passionate about web development, has an appetite for luncheon meats and makes smart
+                        investments in fine ice-cream. In his spare time Hary utilises his creative talents building web
+                        games, experimenting with digital art and playing ultimate frisbee.</p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <div class="about-grid__item--quote homepage-card__heading homepage-card__heading--black">
+            <p>We're a team of makers, thinkers, explorers and theatre singers.<br><br> We approach work and play with
+                curiosity
+                and experimentation, using what we learn to create meaningful digital products that connect with people,
+                just like you.</p>
+        </div>
+        <div class="about-grid__item about-card ">
+            <div class="about-card__side about-card__side--front">
+                <div class="about-card__image"
+                    style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/person5.jpg)"></div>
+                <h3 class="about-card__name">Lily Day</h3>
+                <p class="about-card__job-title">Marketing and PR manager</p>
+            </div>
+            <div class="about-card__side about-card__side--back">
+                <div class="about-card__details">
+
+                    <p>Lover of a good meme, Lily fancies both design and front-end development. She is an avid
+                        basketballer, runner and exercise junkie. Crocheting with a cup of tea in front of the telly?
+                        Yep you'll find her doing that, too.</p>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="about-grid__item about-card ">
+            <div class="about-card__side about-card__side--front">
+                <div class="about-card__image"
+                    style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/person7.jpg)"></div>
+                <h3 class="about-card__name">Sophia Charlton</h3>
+                <p class="about-card__job-title">Account manager</p>
+            </div>
+            <div class="about-card__side about-card__side--back">
+                <div class="about-card__details">
+                    <p>Sophia has a serious design crush but a business brain, so she's naturally found a home at
+                        Impulse flexing her management muscle and keeping the creatives in line. She likes to taunt the
+                        other Humaans by avoiding board game sessions but loves to socialise on the outskirts. She is
+                        the perfect ying, to their nerd yang.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="about-grid__item about-card ">
+            <div class="about-card__side about-card__side--front">
+                <div class="about-card__image"
+                    style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/person2.jpg)"></div>
+                <h3 class="about-card__name">Alex Rahman</h3>
+                <p class="about-card__job-title">Project Manager</p>
+            </div>
+            <div class="about-card__side about-card__side--back">
+                <div class="about-card__details">
+                    <p>Alex has a healthy obsession with all things digital, strongly believing in the intersection of
+                        design and technology. Constantly exploring and prototyping new ideas, he'll jump at the chance
+                        to be involved in any aspect of a project. You may catch him reading Malcolm Gladwell or
+                        watching reruns of The Sopranos.</p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="about-grid__item about-card ">
+            <div class="about-card__side about-card__side--front">
+                <div class="about-card__image"
+                    style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/person6.jpg)"></div>
+                <h3 class="about-card__name">Ashley Christensen</h3>
+                <p class="about-card__job-title">Web & Mobile Developer</p>
+            </div>
+            <div class="about-card__side about-card__side--back">
+                <div class="about-card__details">
+                    <p>Ashley writes and writes. She also makes stuff out of yarn, paper, fabric, clay, electronics, old
+                        boxes and whatever else she can get her hands on. You'll usually find her gardening, gaming,
+                        training or playing a spot of football.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="about-grid__item about-card ">
+            <div class="about-card__side about-card__side--front">
+                <div class="about-card__image"
+                    style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/person4.jpg)"></div>
+                <h3 class="about-card__name">James Forster</h3>
+                <p class="about-card__job-title">Fullstack developer</p>
+            </div>
+            <div class="about-card__side about-card__side--back">
+                <div class="about-card__details">
+                    <p>James is a compulsive tinkerer with technology. He susceptible to long hiccups and when he's not
+                        fishing quadcopters out of a tree he can be found building contraptions with his kids.</p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+        <div class="about-grid__item about-card ">
+            <div class="about-card__side about-card__side--front">
+                <div class="about-card__image"
+                    style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/person3.jpg)"></div>
+                <h3 class="about-card__name">Dominic Dean</h3>
+                <p class="about-card__job-title">Technical SEO manager</p>
+            </div>
+            <div class="about-card__side about-card__side--back">
+                <div class="about-card__details">
+                    <p>Dominic helps our clients achieve their brand vision in the digital space through strategic
+                        advice and streamlined project management. He also helps fellow musicians achieve their band
+                        vision in the analogue space as a freelance bassist. It's kind of the same thing to him.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
 
 
 <?php get_footer();?>
