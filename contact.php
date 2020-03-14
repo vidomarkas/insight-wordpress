cc50720032aa100bc7dea932911f69c6864eb5d9
Viktoras Domarkas <vidomarkas@gmail.com>
2020-03-14 05:08:10 +0000

    corrected contact page layout

--- contact.php
+++ contact.php
@@ -4,22 +4,19 @@
 <div class="contact">
 
     <header id="contact-header" class="header-fixed">
-
         <div class="about__header ">
             <div class="about__header__container space-between">
                 <div class="about__header__person">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/intro-with-people--green-coat.png" alt="">
                 </div>
-
-
                 <div class="about__header__person about__header__person--right">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/intro-with-people--hand-in-pockets.png"
                         alt="">
                 </div>
             </div>
-
         </div>
     </header>
+
     <div class="main-content">
         <div class="about__header__text">
             <h1 class="heading heading__primary--about ">Want to build your very own CROWD?
@@ -49,6 +46,22 @@
 
                 </label>
 
+            </div>
+            <div id="respond">
+                <?php echo $response; ?>
+                <form action="<?php the_permalink(); ?>" method="post">
+                    <p><label for="name">Name: <span>*</span> <br><input type="text" name="message_name"
+                                value="<?php echo esc_attr($_POST['message_name']); ?>"></label></p>
+                    <p><label for="message_email">Email: <span>*</span> <br><input type="text" name="message_email"
+                                value="<?php echo esc_attr($_POST['message_email']); ?>"></label></p>
+                    <p><label for="message_text">Message: <span>*</span> <br><textarea type="text"
+                                name="message_text"><?php echo esc_textarea($_POST['message_text']); ?></textarea></label>
+                    </p>
+                    <p><label for="message_human">Human Verification: <span>*</span> <br><input type="text"
+                                style="width: 60px;" name="message_human"> + 3 = 5</label></p>
+                    <input type="hidden" name="submitted" value="1">
+                    <p><input type="submit"></p>
+                </form>
             </div>
             <hr>
         </form>
